1bcbb181c71c7d504f3e94ae82570b9b92cd50f7
cefadrom <cefadrom1@gmail.com>
2020-01-22 13:32:56 +0100

    Web server made

---
+++ src/server/server.ts
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../web/index.html'));
+});
+
+app.use('/ts', express.static(path.join(__dirname, '../web/ts/')));
+app.use('/css', express.static(path.join(__dirname, '../web/css/')));
+
+app.listen(80);
